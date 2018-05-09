69a921ed0768a33dce41f0a24529d3d56a67c68b
patricklink314 <patricklink314@gmail.com>
2018-05-09 17:14:45 +0200

    Parser

--- src/FileParser.java
+++ src/FileParser.java
@@ -1,4 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileParser {
+    File file;
+    Scanner scanner;
 
+    public FileParser(String path){
+        file = new File(path);
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        scanner.useDelimiter(";");
+    }
 
+    public Location nextLocation(){
+        if(scanner.hasNext()) {
+            String name = scanner.next();
+            float x = scanner.nextFloat();
+            float y = scanner.nextFloat();
+            LocationType type = (scanner.next().equalsIgnoreCase("airport") ? LocationType.AIRPORT : LocationType.TRAINSTATION);
+            return new Location(name, x, y, type);
+        }else {
+            return null;
+        }
+    }
 }
--- src/TeamAufgabe.java
+++ src/TeamAufgabe.java
@@ -2,6 +2,8 @@ public class TeamAufgabe {
 
     public static void main(String[] args) {
 	    System.out.println("Hallo");
+	    FileParser locationFile = new FileParser("./data/junctions.csv");
+	    System.out.println(locationFile.nextLocation());
     }
 }
 
