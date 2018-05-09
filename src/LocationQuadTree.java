4eef2da64e3bc7b57c6efbb88f1119045b781afb
patricklink314 <patricklink314@gmail.com>
2018-05-09 19:43:32 +0200

    Add function implemented in QuadTree and its nodes

---
+++ src/LocationQuadTree.java
@@ -0,0 +1,42 @@
+public class LocationQuadTree implements LocationQuery {
+    QuadTreeNode root;
+
+
+    public LocationQuadTree(float topLeftx, float topLefty, float bottomRightx, float bottomRighty){
+        root = new QuadTreeNode(topLeftx,topLefty,bottomRightx,bottomRighty);
+    }
+    /**
+     * Adds given location
+     *
+     * @param location
+     */
+    @Override
+    public void add(Location location) {
+        if(root !=null){
+            root.add(location);
+        }else{
+            root = new QuadTreeNode(location,-100000.0f,-100000.0f,100000.0f,100000.0f);
+        }
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @param radius
+     * @return an array with 2 elements, where the n-th element is the occurrence of locations with the type of the n-th enum value, which are inside the given circle
+     */
+    @Override
+    public int[] locationsInArea(float x, float y, float radius) {
+        return new int[0];
+    }
+
+    /**
+     * @param stationCount
+     * @param radius
+     * @return the amount of airports, that have at least stationCount Train stations located in the given radius
+     */
+    @Override
+    public int airportsWithNStations(int stationCount, float radius) {
+        return 0;
+    }
+}
--- src/QuadTreeNode.java
+++ src/QuadTreeNode.java
@@ -13,15 +13,81 @@ public class QuadTreeNode {
     float bottomRightx;
     float bottomRighty;
 
-    public QuadTreeNode(float topLeftx, float topLefty,float bottomRightx,float bottomRighty){
-        this.topLeftx=topLeftx;
-        this.topLefty=topLefty;
-        this.bottomRightx=bottomRightx;
-        this.bottomRighty=bottomRighty;
+    //flag. Is true if node is leaf;
+    boolean leafFlag;
+
+    public QuadTreeNode(float topLeftx, float topLefty, float bottomRightx, float bottomRighty) {
+        this.topLeftx = topLeftx;
+        this.topLefty = topLefty;
+        this.bottomRightx = bottomRightx;
+        this.bottomRighty = bottomRighty;
     }
 
-    public QuadTreeNode(Location value, float topLeftx, float topLefty,float bottomRightx,float bottomRighty){
+    public QuadTreeNode(Location value, float topLeftx, float topLefty, float bottomRightx, float bottomRighty) {
         this(topLeftx, topLefty, bottomRightx, bottomRighty);
-        this.value=value;
+        this.value = value;
+        leafFlag = true;
+    }
+
+    public Location getValue() {
+        return value;
+    }
+
+    public boolean add(Location loc) {
+        if (loc == null) {
+            return false;
+        }
+        if (!inBoundry(loc)) {
+            return false;
+        }
+        if (leafFlag) {
+            //split current node into 4 regions and pass value to correct subregion
+            //x-split
+            splitNode(value);
+            leafFlag = false;
+        }
+        splitNode(loc);
+        return true;
+    }
+
+    public boolean inBoundry(Location loc) {
+        return (topLeftx <= loc.getX()
+                && topLefty <= loc.getY()
+                && bottomRightx >= loc.getX()
+                && bottomRighty >= loc.getY());
+    }
+
+    private void splitNode(Location loc) {
+        float xSplit = (topLeftx + bottomRightx) / 2;
+        float ySplit = (topLefty + bottomRighty) / 2;
+        if (xSplit >= loc.getX()) { //left
+            if (ySplit <= loc.getY()) { //top
+                if (topLeft == null) {
+                    topLeft = new QuadTreeNode(loc, topLeftx, topLefty, xSplit, ySplit);
+                } else {
+                    topLeft.add(loc);
+                }
+            } else {//bottom
+                if (bottomRight == null) {
+                    bottomLeft = new QuadTreeNode(loc, topLeftx, ySplit, xSplit, bottomRighty);
+                } else {
+                    bottomLeft.add(loc);
+                }
+            }
+        } else { //right
+            if (ySplit <= loc.getY()) { //top
+                if (topRight == null) {
+                    topRight = new QuadTreeNode(loc, xSplit, topLefty, bottomRightx, ySplit);
+                } else {
+                    topRight.add(loc);
+                }
+            } else {//bottom
+                if (bottomRight == null) {
+                    bottomRight = new QuadTreeNode(loc, xSplit, ySplit, bottomRightx, bottomRighty);
+                } else {
+                    bottomRight.add(loc);
+                }
+            }
+        }
     }
 }
