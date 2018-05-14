daa68743848daf941045731cde8fdbcc44efd5d7
Simon Reiser <ein.simonreiser@gmail.com>
2018-05-14 17:34:45 +0200

    Removed file read time from the initialization tests

--- src/TeamAufgabe.java
+++ src/TeamAufgabe.java
@@ -1,44 +1,56 @@
 import java.io.FileNotFoundException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 
 public class TeamAufgabe {
 
     public static void main(String[] args) {
+        ArrayList<Location> locations = readLocationsFromFile();
+        
 	    LocationArrayList list = new LocationArrayList();
 	    LocationQuadTree quadTree = new LocationQuadTree(-100000.0f,100000.0f,100000.0f,-100000.0f);
 	    Location2DTree tree2D = new Location2DTree();
 
-	    fillDatastructure(list);
+	    fillDatastructure(locations, list);
 	    testLocationCount(list);
 	    testAirportsWithNStations(list);
 
-        fillDatastructure(tree2D);
+        fillDatastructure(locations,tree2D);
         testLocationCount(tree2D);
         testAirportsWithNStations(tree2D);
 
 	    //QuadTree test
-        fillDatastructure(quadTree);
+        fillDatastructure(locations,quadTree);
         testLocationCount(quadTree);
         testAirportsWithNStations(quadTree);
     }
 
-    public static void fillDatastructure(LocationQuery lq)
+    public static ArrayList<Location> readLocationsFromFile()
     {
+        ArrayList<Location> list = new ArrayList<>();
         try {
             FileParser locationFile = new FileParser("./data/junctions.csv");
 
-            Instant start = Instant.now();
-
             Location location;
             while((location=locationFile.nextLocation())!=null)
-                lq.add(location);
-
-            Instant end = Instant.now();
-            System.out.println("Initialization of "+lq.getClass().getName()+" needed "+Duration.between(start,end).toMillis()+"ms");
+                list.add(location);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        return list;
+    }
+
+    public static void fillDatastructure(ArrayList<Location> locations, LocationQuery lq)
+    {
+        Instant start = Instant.now();
+
+        for(Location location : locations)
+            lq.add(location);
+
+        Instant end = Instant.now();
+        System.out.println("Initialization of "+lq.getClass().getName()+" needed "+Duration.between(start,end).toMillis()+"ms");
     }
 
     public static void testLocationCount(LocationQuery lq)
